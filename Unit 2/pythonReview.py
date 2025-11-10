7b81bf59b916df9dff54ac0abace669de72de7d5
NolanH7rb <nhall255@boyslatin.net>
2025-11-10 16:10:30 +0000

    finished monday assignment

---
+++ Unit 2/pythonReview.py
@@ -0,0 +1,55 @@
+
+# Question 1
+# Build a program that determines if a student has submitted their class work 
+# and homework assignment. The program should use an operator that allows 
+# for evaluating 2 conditions and determining if the conditions are true 
+# or false.
+
+# hint: find the a operator that allows you to evaluate 2 condtions.
+
+Homework = True
+Classwork = True
+if Homework and Classwork:
+        
+        print('These Assignments have been submitted!')
+
+
+# Question 2
+# Create a function that will take in a string as an argument and output 
+# that string in reverse order.
+
+def ReverseITnow(x):
+  return x[::-1]
+
+mytxt = ReverseITnow("Mr Ian is the Goat of Coding")
+
+print(mytxt)
+# hint: look into string reverse in w3schools
+
+# Question 3
+# Create a number guessing function where the program will continuously 
+# ask the user to enter a number until the guess the number correctly. 
+# Your program should also give the user information on if their guess 
+# is close to the correct number. If the guess is above the correct number 
+# it should tell the user it is too high and try again. 
+# If the guess is below the number, it should tell the user it is too low, 
+# it should tell them it is too low and to guess again. Once the user gets 
+# it correct the program should congratulate them, stop, and tell them how 
+# many attempts they made.
+
+def GuessThat():
+    x = 7
+    guess= input('what is the secret number')
+    while guess != x:
+        guess= input('what is the secret number')
+        if  guess < x:
+            print('Your close! Go Higher')
+            guess= input('what is the secret number')
+
+        elif    guess > x:
+            print('Your close! Go Lower')
+    
+    else:
+         print('CorrectoMundo')
+
+GuessThat()
